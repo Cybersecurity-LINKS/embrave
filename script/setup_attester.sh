7645ae0341c925044a49a55b5e87fc588e60bc8f
AleCla97 <alessio.claudio@linksfoundation.com>
2023-11-07 15:07:30 +0100

    rename an change scripts

--- script/setup_attester.sh
+++ script/setup_attester.sh
@@ -11,7 +11,7 @@ sudo ./script/create_certificates.sh
 sudo python3 ./script/goldenvalue_db_generator.py sha256 /
 
 #copy the file to send
-cp ./Agents/Remote_Attestor/AKs/ak.pub.pem ./to_send/
-cp ./Server/certs/server.crt ./to_send/
-cp ./Protocols/Explicit/goldenvalues.db ./to_send/
-cp ./Server/certs/ca.crt ./to_send/
+#cp ./Agents/Remote_Attestor/AKs/ak.pub.pem ./to_send/
+#cp ./Server/certs/server.crt ./to_send/
+#cp ./Protocols/Explicit/goldenvalues.db ./to_send/
+#cp ./Server/certs/ca.crt ./to_send/
---
+++ script/setup_verifier.sh
@@ -0,0 +1,13 @@
+
+#scp -pr root@192.168.85.203:/sources/lemon/to_send ./to_receive
+LEMONDIR=/home/ale/Scrivania/lemon
+#cp ./to_receive/to_send/ak.pub.pem ./Agents/Remote_Attestor/AKs/ak.pub.imx.pem
+#cp ./to_receive/to_send/server.crt ./Server/certs/server.imx.crt
+#mv ./to_receive/to_send/goldenvalues.db ./Protocols/Explicit/goldenvalues.imx.db
+#cp ./to_receive/to_send/ca.crt ./Server/certs/ca.imx.crt
+#create sha256 ak digest
+var=$(openssl dgst -sha256 ${LEMONDIR}/certs/ak.pub.pem)
+sha256=$(echo "${var#*= }")
+
+#create remote attestor binding database
+python3 ./script/verifier_db.py $sha256 ${LEMONDIR}/certs/ak.pub.pem ${LEMONDIR}/certs/server.crt file:${LEMONDIR}/certs/goldenvalues.db ${LEMONDIR}/certs/ca.imx.crt
