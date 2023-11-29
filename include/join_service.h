2b34f173656dfb0989b485af802be27c1be60312
Enrico Bravi <enrico.bravi@polito.it>
2023-11-29 18:20:12 +0100

    join_service: add join_service http server structure

---
+++ include/join_service.h
@@ -0,0 +1,21 @@
+#ifndef __JOIN_SERVICE__
+#define __JOIN_SERVICE__
+
+#include "mongoose.h"
+
+/* methods */
+#define GET "GET"
+#define POST "POST"
+#define PUT "PUT"
+#define DELETE "DELETE"
+
+/* codes */
+#define OK 200
+
+/* responses */
+#define APPLICATION_JSON "Content-Type: application/json\r\n"
+
+/* APIs */
+#define API_JOIN "/join"
+
+#endif
