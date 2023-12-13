b6b205a4d7a2fac4e1aac8cbbe4e9fcd33a55315
AleCla97 <alessio.claudio@linksfoundation.com>
2023-12-13 16:00:56 +0100

    add missing license header

--- src/attester/tpm_ak.c
+++ src/attester/tpm_ak.c
@@ -1,3 +1,15 @@
+// Copyright (C) 2023 Fondazione LINKS 
+
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with this program; if not, 
+// write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 #include "tpm_ak.h"
 
 tool_rc init_ak_public(TPMI_ALG_HASH name_alg, TPM2B_PUBLIC *public, struct createak_context *ctx) {
