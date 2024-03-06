0b0280d0e77d90929c40cd189c2576d6de29bb9e
AleCla97 <alessio.claudio97@gmail.com>
2024-03-06 10:39:57 +0000

    update script

--- scripts/process_simulation.sh
+++ scripts/process_simulation.sh
@@ -1,9 +1,15 @@
-for ((i=1; i<=1000; i++)) #sudo valgrind --leak-check=yes
+#Simulation of a process that extends the IMA log
+#Used to test the incremental IMA log verification
+#This script sleeps for a random timer between T_MIX and TMAX+1 and the execute some istructions to extend the IMA log
+T_MIX=2
+T_MAX=8
+N=1000
+for ((i=1; i<=$N; i++)) #sudo valgrind --leak-check=yes
 do
-    var=$(( ($RANDOM % 60) + 2 ))
+    var=$(( ($RANDOM % $T_MAX) + $T_MIX ))
     echo "Sleep: $var"
     sleep $var
     sudo date > tmp.txt
-    sudo cat tmp.txt > aaa
-    sudo rm tmp.txt aaa
+    sudo cat tmp.txt > tmp2
+    sudo rm tmp.txt tmp2
 done
